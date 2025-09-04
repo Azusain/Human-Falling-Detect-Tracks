e00c7ed63a881e04b77d4b550d4af0fe547410cb
Azusain <azusaings@gmail.com>
2025-09-04 20:11:03 +0800

    chore: restore originl image scale

--- CameraLoader.py
+++ CameraLoader.py
@@ -1,11 +1,9 @@
-import os
 import cv2
 import time
-import torch
-import numpy as np
 
-from queue import Queue
 from threading import Thread, Lock
+
+from sympy import true
 
 
 class CamLoader:
@@ -18,7 +16,8 @@ class CamLoader:
     """
     def __init__(self, camera, preprocess=None, ori_return=False):
         self.stream = cv2.VideoCapture(camera)
-        assert self.stream.isOpened(), 'Cannot read camera source!'
+        if not self.stream.isOpened():
+            self.valid = False
         self.fps = self.stream.get(cv2.CAP_PROP_FPS)
         self.frame_size = (int(self.stream.get(cv2.CAP_PROP_FRAME_WIDTH)),
                            int(self.stream.get(cv2.CAP_PROP_FRAME_HEIGHT)))
@@ -29,7 +28,7 @@ class CamLoader:
         self.ori_frame = None
         self.read_lock = Lock()
         self.ori = ori_return
-
+        self.valid = True
         self.preprocess_fn = preprocess
 
     def start(self):
@@ -86,82 +85,3 @@ class CamLoader:
             self.stream.release()
 
 
-class CamLoader_Q:
-    """Use threading and queue to capture a frame and store to queue for pickup in sequence.
-    Recommend for video file.
-
-    Args:
-        camera: (int, str) Source of camera or video.,
-        batch_size: (int) Number of batch frame to store in queue. Default: 1,
-        queue_size: (int) Maximum queue size. Default: 256,
-        preprocess: (Callable function) to process the frame before return.
-    """
-    def __init__(self, camera, batch_size=1, queue_size=256, preprocess=None):
-        self.stream = cv2.VideoCapture(camera)
-        assert self.stream.isOpened(), 'Cannot read camera source!'
-        self.fps = self.stream.get(cv2.CAP_PROP_FPS)
-        self.frame_size = (int(self.stream.get(cv2.CAP_PROP_FRAME_WIDTH)),
-                           int(self.stream.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-
-        # Queue for storing each frames.
-
-        self.stopped = False
-        self.batch_size = batch_size
-        self.Q = Queue(maxsize=queue_size)
-
-        self.preprocess_fn = preprocess
-
-    def start(self):
-        t = Thread(target=self.update, args=(), daemon=True).start()
-        c = 0
-        while not self.grabbed():
-            time.sleep(0.1)
-            c += 1
-            if c > 20:
-                self.stop()
-                raise TimeoutError('Can not get a frame from camera!!!')
-        return self
-
-    def update(self):
-        while not self.stopped:
-            if not self.Q.full():
-                frames = []
-                for k in range(self.batch_size):
-                    ret, frame = self.stream.read()
-                    if not ret:
-                        self.stop()
-                        return
-
-                    if self.preprocess_fn is not None:
-                        frame = self.preprocess_fn(frame)
-
-                    frames.append(frame)
-                    frames = np.stack(frames)
-                    self.Q.put(frames)
-            else:
-                with self.Q.mutex:
-                    self.Q.queue.clear()
-
-    def grabbed(self):
-        """Return `True` if can read a frame."""
-        return self.Q.qsize() > 0
-
-    def getitem(self):
-        return self.Q.get().squeeze()
-
-    def stop(self):
-        if self.stopped:
-            return
-        self.stopped = True
-        self.stream.release()
-
-    def __len__(self):
-        return self.Q.qsize()
-
-    def __del__(self):
-        if self.stream.isOpened():
-            self.stream.release()
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        if self.stream.isOpened():
-            self.stream.release()
